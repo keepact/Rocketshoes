4cb32fd446e6b304652633131ad6be2be2f908f1
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-10 00:01:05 -0300

    Animations with Lottie

--- web/src/pages/Cart/index.js
+++ web/src/pages/Cart/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   MdRemoveCircleOutline,
@@ -6,17 +6,23 @@ import {
   MdDelete,
 } from 'react-icons/md';
 
+import Animation from '../../components/Animation';
+import emptyAnimation from '../../assets/animations/empty-cart.json';
+import successAnimation from '../../assets/animations/sending-success.json';
+
 import { formatPrice } from '../../util/format';
 
 import * as CartActions from '../../store/modules/cart/actions';
 
-import { Container, ProductTable, Total } from './styles';
+import { Container, ProductTable, Total, AnimationContainer } from './styles';
 
 export default function Cart() {
+  const [finished, setFinished] = useState(false);
+
   const total = useSelector(state =>
     formatPrice(
-      state.cart.reduce((total, product) => {
-        return total + product.price * product.amount;
+      state.cart.reduce((sumTotal, product) => {
+        return sumTotal + product.price * product.amount;
       }, 0)
     )
   );
@@ -38,65 +44,103 @@ export default function Cart() {
     dispatch(CartActions.updateAmountRequest(product.id, product.amount - 1));
   }
 
+  function handleFinhesed() {
+    setFinished(true);
+
+    window.setTimeout(() => {
+      window.location.href = 'http://localhost:3000';
+    }, 2500);
+  }
+
   return (
     <Container>
-      <ProductTable>
-        <thead>
-          <tr>
-            <th />
-            <th>PRODUTO</th>
-            <th>QTD</th>
-            <th>SUBTOTAL</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map(product => (
-            <tr>
-              <td>
-                <img src={product.image} alt={product.title} />
-              </td>
-              <td>
-                <strong>{product.title}</strong>
-                <span>{product.priceFormatted}</span>
-              </td>
-              <td>
-                <div>
-                  <button type="button" onClick={() => decrement(product)}>
-                    <MdRemoveCircleOutline size={20} color="#7159c1" />
-                  </button>
-                  <input type="number" readOnly value={product.amount} />
-                  <button type="button" onClick={() => increment(product)}>
-                    <MdAddCircleOutline size={20} color="#7159c1" />
-                  </button>
-                </div>
-              </td>
-              <td>
-                <strong>{product.subtotal}</strong>
-              </td>
-              <td>
-                <button
-                  type="button"
-                  onClick={() =>
-                    dispatch(CartActions.removeFromCart(product.id))
-                  }
-                >
-                  <MdDelete size={20} color="#7159c1" />
+      {finished ? (
+        <AnimationContainer>
+          <Animation animation={successAnimation} size={200} autoplay={false} />
+          <h2>Sua compra foi processada, obrigado pela confiança!</h2>
+        </AnimationContainer>
+      ) : (
+        <>
+          {cart.length > 0 ? (
+            <>
+              <ProductTable>
+                <thead>
+                  <tr>
+                    <th />
+                    <th>PRODUTO</th>
+                    <th>QTD</th>
+                    <th>SUBTOTAL</th>
+                    <th />
+                  </tr>
+                </thead>
+                <tbody>
+                  {cart.map(product => (
+                    <tr>
+                      <td>
+                        <img src={product.image} alt={product.title} />
+                      </td>
+                      <td>
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormatted}</span>
+                      </td>
+                      <td>
+                        <div>
+                          <button
+                            type="button"
+                            onClick={() => decrement(product)}
+                          >
+                            <MdRemoveCircleOutline size={20} color="#7159c1" />
+                          </button>
+                          <input
+                            type="number"
+                            readOnly
+                            value={product.amount}
+                          />
+                          <button
+                            type="button"
+                            onClick={() => increment(product)}
+                          >
+                            <MdAddCircleOutline size={20} color="#7159c1" />
+                          </button>
+                        </div>
+                      </td>
+                      <td>
+                        <strong>{product.subtotal}</strong>
+                      </td>
+                      <td>
+                        <button
+                          type="button"
+                          onClick={() =>
+                            dispatch(CartActions.removeFromCart(product.id))
+                          }
+                        >
+                          <MdDelete size={20} color="#7159c1" />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </ProductTable>
+
+              <footer>
+                <button type="button" onClick={() => handleFinhesed()}>
+                  Finalizar pedido
                 </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </ProductTable>
 
-      <footer>
-        <button type="button">Finalizar pedido</button>
-
-        <Total>
-          <span>TOTAL</span>
-          <strong>{total}</strong>
-        </Total>
-      </footer>
+                <Total>
+                  <span>TOTAL</span>
+                  <strong>{total}</strong>
+                </Total>
+              </footer>
+            </>
+          ) : (
+            <AnimationContainer>
+              <Animation animation={emptyAnimation} size={300} autoplay loop />
+              <h2>Seu carrinho está vazio.</h2>
+            </AnimationContainer>
+          )}
+        </>
+      )}
     </Container>
   );
 }
--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -4,12 +4,17 @@ import { MdShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
+import loadingAnimation from '../../assets/animations/loading.json';
+import Animation from '../../components/Animation';
+
 import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const amount = useSelector(state =>
     state.cart.reduce((sumAmount, product) => {
       sumAmount[product.id] = product.amount;
@@ -22,6 +27,8 @@ export default function Home() {
 
   useEffect(() => {
     async function loadProducts() {
+      setLoading(true);
+
       const response = await api.get('products');
 
       const data = response.data.map(product => ({
@@ -30,6 +37,7 @@ export default function Home() {
       }));
 
       setProducts(data);
+      setLoading(false);
     }
 
     loadProducts();
@@ -40,23 +48,37 @@ export default function Home() {
   }
 
   return (
-    <ProductList>
-      {products.map(product => (
-        <li key={product.id}>
-          <img src={product.image} alt={product.title} />
-          <strong>{product.title}</strong>
-          <span>{product.priceFormatted}</span>
+    <>
+      {loading ? (
+        <Animation
+          animation={loadingAnimation}
+          size={380}
+          autoplay={false}
+          loop={false}
+        />
+      ) : (
+        <ProductList>
+          {products.map(product => (
+            <li key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <strong>{product.title}</strong>
+              <span>{product.priceFormatted}</span>
 
-          <button type="button" onClick={() => handleAddProduct(product.id)}>
-            <div>
-              <MdShoppingCart size={16} color="#FFF" />{' '}
-              {amount[product.id] || 0}
-            </div>
+              <button
+                type="button"
+                onClick={() => handleAddProduct(product.id)}
+              >
+                <div>
+                  <MdShoppingCart size={16} color="#FFF" />{' '}
+                  {amount[product.id] || 0}
+                </div>
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-      ))}
-    </ProductList>
+                <span>ADICIONAR AO CARRINHO</span>
+              </button>
+            </li>
+          ))}
+        </ProductList>
+      )}
+    </>
   );
 }
